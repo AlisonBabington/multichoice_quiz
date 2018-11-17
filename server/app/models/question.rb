56fde97763f7532265aec4d1734156db03ee14e6
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-17 15:21:52 +0000

    rspec for question and answer created

--- server/app/models/question.rb
+++ server/app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+    has_many :answer, dependent: :destroy
+    validates_presence_of :question
 end
--- server/spec/models/question_spec.rb
+++ server/spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #ensure relationship with answers table
+  it {should have_many(:answer).dependent(:destroy)}
+  #ensure question column is there before saving
+  it { should validate_presence_of(:question)}
 end
+
+
+
