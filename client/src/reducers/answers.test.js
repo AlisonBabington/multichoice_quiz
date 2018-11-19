0e6ebeda676eae1de0ffb32cc3cefc26bc98b7d4
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-19 11:18:37 +0000

    typo fixed

---
+++ client/src/reducers/answers.test.js
@@ -0,0 +1,12 @@
+import * as types from '../actiontypes/quiz';
+import answers from './answers';
+
+// describe('answer reducer', () => {
+//   it('should return the initial state', () => {
+//     expect(answers(undefined, {})).toEqual([
+//       {
+        
+//       }
+//     ])
+//   })
+// })
