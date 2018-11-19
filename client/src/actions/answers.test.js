c656497eebe21f4d9b691409b5f6431d78d385b9
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-19 03:35:40 +0000

    test actions completed

--- client/src/actions/answers.test.js
+++ client/src/actions/answers.test.js
@@ -1,13 +1,49 @@
-import * as actions from './answers';
-import * as types from '../actiontypes/quiz'
+import * as answerActions from './answers';
+import * as questionActions from './questions';
+import * as quizActions from './quizzes';
+import * as userActions from './users';
+import * as types from '../actiontypes/quiz';
 
-describe('actions', () => {
+describe('answerActions', () => {
   it('return an array of answers', () => {
     const answers = ["one", "two", "three"]
     const expectedAction = {
       type: types.ANSWERS_LOADED,
       answers
     }
-    expect(actions.setAnswersData(answers)).toEqual(expectedAction)
+    expect(answerActions.setAnswersData(answers)).toEqual(expectedAction)
   })
 })
+
+describe('questionActions', () => {
+  it('return an array of questions', () => {
+    const questions = ["one", "two", "three"]
+    const expectedAction = {
+      type: types.QUESTIONS_LOADED,
+      questions
+    }
+    expect(questionActions.setQuestionData(questions)).toEqual(expectedAction)
+  })
+})
+
+describe('quizActions', () => {
+  it('returns the chosen answer', () => {
+    const option = "hello"
+    const expectedAction = {
+      type: types.ANSWER_MARKED,
+      option
+    }
+    expect(quizActions.updateScore(option)).toEqual(expectedAction)
+  })
+})
+  
+  describe('userActions', () => {
+    it('returns the inputted name', () => {
+      const name = "James"
+      const expectedAction = {
+        type: types.USER_NAME_INPUTTED,
+        name
+      }
+      expect(userActions.userNameInput(name)).toEqual(expectedAction)
+    })
+  })
