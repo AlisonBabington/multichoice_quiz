12331c3c1161476607073989273ff45f35f553f9
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-19 10:26:27 +0000

    home link button add4ed on end screen

--- client/src/components/EndGameBox.js
+++ client/src/components/EndGameBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box } from 'bloomer/lib/elements/Box';
+import { Button } from 'bloomer/lib/elements/Button';
 
 const EndGameBox = ({user}) => {
 
@@ -8,6 +9,7 @@ const EndGameBox = ({user}) => {
             <Box style={{margin: 250}}>
                 <h1>Game over!</h1>
                 <h3>Well done {user.name}, you scored {user.score} points!</h3>
+                <Button type="submit" isColor='info' href="/">Return to Home</Button> 
             </Box>
         </div>
     )
